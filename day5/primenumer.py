d0ae6877588a03d48cc6724005a07dc96fa3811c
Md.Mohtasim Shariar <onim04@gmail.com>
2024-05-21 22:51:26 +0600

    day5 push

--- day5/primenumer.py
+++ day5/primenumer.py
@@ -8,4 +8,21 @@ for i in range(2,101):
                           prime=False
                           break
          if prime:
-                  print(i)          
+                  print(i)    
+
+#For a given number, Check whether the number is a prime number or not.
+def is_prime(num):
+   for i in range(2,num):
+       if (num % i) == 0:
+           return False
+   return True
+
+
+num = int(input("Enter a number: "))
+
+check_prime = is_prime(num)
+
+if check_prime:
+   print('Your number is a Prime')
+else:
+   print('Your number is not a Prime')
